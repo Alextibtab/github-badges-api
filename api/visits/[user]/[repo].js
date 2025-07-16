670366b2f5bad3cd3e9c4f1cc9a699bde205aa97
Alextibtab <14alexcockburn@gmail.com>
2025-07-16 01:52:10 +0100

    hopefully fixed caching issues

--- api/visits/[user]/[repo].js
+++ api/visits/[user]/[repo].js
@@ -7,6 +7,9 @@ export default async function handler(req, res) {
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
 
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method Not Allowed' });
